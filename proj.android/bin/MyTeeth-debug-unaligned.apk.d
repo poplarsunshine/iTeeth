8ba23685e3835c4a0267ed9e22625bcb4818321e
poplarsunshine <Penny@PennydeMacBook-Pro.local>
2015-06-03 08:20:35 +0800

    bin

---
+++ proj.android/bin/MyTeeth-debug-unaligned.apk.d
@@ -0,0 +1,5 @@
+/Users/Penny/Documents/Fat-workspace/project/iTeeth/proj.android/bin/MyTeeth-debug-unaligned.apk : \
+/Users/Penny/Documents/Fat-workspace/project/iTeeth/proj.android/bin/MyTeeth.ap_ \
+/Users/Penny/Documents/Fat-workspace/project/iTeeth/proj.android/bin/classes.dex \
+/Users/Penny/Documents/Fat-workspace/project/iTeeth/proj.android/libs/armeabi/gdbserver \
+/Users/Penny/Documents/Fat-workspace/project/iTeeth/proj.android/libs/armeabi/libcocos2dcpp.so \
